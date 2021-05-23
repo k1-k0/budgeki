22fad16e2dc02bb855044aba7c5925729878dc08
k1-k0 <kmssrvk@gmail.com>
2021-05-23 22:29:45 +0300

    add rest routes for user

--- main.py
+++ main.py
@@ -31,5 +31,21 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
 
+@app.get('/users/', response_model=schemas.User)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = crud.get_user(db, user_id=user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail=f'User with id {user_id} not exists')
+    return user
+
+
+@app.get('/users/', response_model=schemas.User)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = crud.get_user(db, user_id=user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail=f'User with id {user_id} not exists')
+    return user
+
+
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=42000)
